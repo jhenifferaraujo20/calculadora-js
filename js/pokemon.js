df11d1c4ffa10629fbd0a584022dc86b51937acb
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2021-03-31 22:37:35 -0300

    Calculadora

---
+++ js/pokemon.js
@@ -0,0 +1,15 @@
+let primeiroNumero = alert("Quem é esse Pokémon?");
+
+let pokemon = parseInt(prompt("Digite 1 para Charmander, 2 para Squirtle, 3 para Pikachu e 4 para Nidorina"));
+
+if(pokemon == 1){
+    document.write("<h2> Errou, tente novamente </h2>");
+}else if(pokemon == 2){
+    document.write("<h2> Errou, tente novamente </h2>");
+}
+else if(pokemon == 3){
+    document.write("<h2> Parabéns, você acertou </h2>");
+}
+else if(pokemon == 4){
+    document.write("<h2> Errou, tente novamente </h2>");
+}
